e1594c003ac69993554b672b9efc7d3949cb91ca
Lucia <lucia.rapanova@gmail.com>
2020-09-12 18:10:56 +0200

    dokonceny homepage, animacie

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 50)  {
+            $('.navbar').addClass('nav-scroll-style');
+        }
+
+        if($(this).scrollTop() < 50)  {
+            $('.navbar').removeClass('nav-scroll-style');
+        }
+    });
+});
